f6718ba7267eb63da401387bce5bc301e6b8b19d
poojanihirunima <poojanihirunima@gmail.com>
2026-01-31 11:46:11 +0530

    implemented a simple UI

---
+++ ui.py
@@ -0,0 +1,19 @@
+import gradio as gr
+
+from app import execute_graph
+
+
+def chat(query):
+    return execute_graph(query)
+
+
+ui = gr.Interface(
+    fn=chat,
+    inputs=[gr.Textbox(lines=2, placeholder="Anything  you  curious  about  today?...")],
+    outputs=gr.Textbox(lines=10),
+    title="Personal AI Assistant 👋🏼",
+    description="Hey there ;) Your Personal Assistant is here Ask me anything"
+)
+
+ui.launch()
+
